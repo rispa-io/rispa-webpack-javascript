400d4739643cba8f3631461e5d7af90db69ce54a
Maxim Samoilov <samoilowmaxim@gmail.com>
2017-06-16 14:37:30 +0300

    Initial commint

---
+++ .rispa/activator.js
@@ -0,0 +1,17 @@
+import { init, start, build } from '@rispa/core/events'
+import { server } from '@rispa/server/events'
+import runBuild from '../src/build'
+import webpackExtensionCommon from './common.wpc'
+import webpackExtensionClient from './client.wpc'
+
+const activator = on => {
+  const initHandler = registry => {
+    registry.add('webpack.common', webpackExtensionCommon)
+    registry.add('webpack.client', webpackExtensionCommon, webpackExtensionClient)
+  }
+
+  on(init(build), initHandler)
+  on(init(server), initHandler)
+}
+
+export default activator

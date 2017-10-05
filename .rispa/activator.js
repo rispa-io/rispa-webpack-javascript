2b4efbb03c6e8a1d005de3ff720823e4a8b08b6f
dvpetrov <dvpetrov.main@gmail.com>
2017-10-05 18:46:52 +0300

    Initial impl. new specs

--- .rispa/activator.js
+++ .rispa/activator.js
@@ -1,17 +1,11 @@
-import { init, build } from '@rispa/core/events'
-import { server } from '@rispa/server/events'
-import getWebpackJavascriptConfig from './javascript.wpc'
-import getBabelOptions from './babel-options'
+const WebpackJavaScriptPlugin = require('../src/WebpackJavaScriptPlugin')
 
-const activator = on => {
-  const initHandler = registry => {
-    registry.add('webpack.common', getWebpackJavascriptConfig(registry))
-    registry.add('webpack.client', getWebpackJavascriptConfig(registry))
-    registry.add('babel', getBabelOptions)
-  }
-
-  on(init(build), initHandler)
-  on(init(server), initHandler)
+function init(context, config) {
+  return new WebpackJavaScriptPlugin(context, config)
 }
 
-export default activator
+const after = ['@rispa/webpack', '@rispa/babel']
+
+module.exports = init
+
+module.exports.after = after

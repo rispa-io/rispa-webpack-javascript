657766029026bf16809a40bc9c1c31c13e41a486
Dmitry Petrov <dvpetrov.main@gmail.com>
2018-12-27 05:04:25 +0300

    Rewrok polyfill bundle

--- src/configs/client.wpc.js
+++ src/configs/client.wpc.js
@@ -1,21 +1,9 @@
-const polyfill = require.resolve('@babel/polyfill')
+const polyfill = require.resolve('./polyfill')
 
 module.exports = (context, { merge }) => merge({
   entry: {
-    polyfill: !process.env.DISABLE_POLYFILL ? [
+    vendors: !process.env.DISABLE_POLYFILL ? [
       polyfill,
     ] : [],
-  },
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        polyfill: {
-          name: 'polyfill',
-          chunks: 'all',
-          test: module => module.rawRequest === polyfill,
-          priority: 20,
-        },
-      },
-    },
   },
 })
